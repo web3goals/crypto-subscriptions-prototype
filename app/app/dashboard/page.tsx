0f0503fa28092696aa692f6239281bc891a3d6f5
kiv1n <aloha@kiv1n.ru>
2024-04-01 17:42:26 +0300

    Implement form to create a product

---
+++ app/app/dashboard/page.tsx
@@ -0,0 +1,20 @@
+import { Button } from "@/components/ui/button";
+import { Separator } from "@/components/ui/separator";
+import Link from "next/link";
+
+export default function DashboardPage() {
+  return (
+    <div className="container py-10 lg:px-80">
+      <div className="space-y-0.5">
+        <h2 className="text-2xl font-bold tracking-tight">Dashboard</h2>
+        <p className="text-muted-foreground">
+          Products you offer to Web3 users
+        </p>
+      </div>
+      <Separator className="my-6" />
+      <Link href="/dashboard/products/new">
+        <Button>Create Product</Button>
+      </Link>
+    </div>
+  );
+}
--- app/config/site.ts
+++ app/config/site.ts
@@ -11,6 +11,7 @@ export const siteConfig = {
   },
   contracts: {
     chain: arbitrumSepolia,
-    product: "0x0000000000000000000000000000000000000000" as `0x${string}`,
+    product: "0x02008a8DBc938bd7930bf370617065B6B0c1221a" as `0x${string}`,
+    usdt: "0x96E6AF6E9e400d0Cd6a4045F122df22BCaAAca59" as `0x${string}`,
   },
 };
